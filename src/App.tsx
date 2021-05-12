15f45f12cb6ff4df84f68429f425225d0e88b8bc
Bo Aanes <boaanes@gmail.com>
2021-05-12 21:59:51 +0200

    Første forpliktelse

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import bo from './bo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (): JSX.Element => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={bo} className="App-logo" alt="logo" />
+                <p>aanes.dev</p>
+                <a href="https://github.com/boaanes">GitHub</a>
+            </header>
+        </div>
+    );
+};
 
 export default App;
