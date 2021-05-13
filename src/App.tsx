fc5dde1484e38f34b71e95160215d1c0835d11c5
Bo Aanes <boaanes@gmail.com>
2021-05-13 15:29:13 +0200

    added chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
 import bo from './bo.png';
-import './App.css';
+import { Box, ChakraProvider, Flex, Image, Text, keyframes, usePrefersReducedMotion, Link } from '@chakra-ui/react';
+
+const spin = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+`;
 
 const App = (): JSX.Element => {
+    const prefersReducedMotion = usePrefersReducedMotion();
+    const animation = prefersReducedMotion ? undefined : `${spin} infinite 20s linear`;
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={bo} className="App-logo" alt="logo" />
-                <p>aanes.dev</p>
-                <a href="https://github.com/boaanes" className="App-link">
-                    GitHub
-                </a>
-            </header>
-        </div>
+        <ChakraProvider>
+            <Box textAlign="center">
+                <Flex
+                    bg="#282c34"
+                    minH="100vh"
+                    flexDir="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    fontSize="calc(10px + 2vmin)"
+                    color="white"
+                >
+                    <Image
+                        src={bo}
+                        animation={animation}
+                        height="40vmin"
+                        pointerEvents="none"
+                        borderRadius="100%"
+                        alt="me"
+                    />
+                    <Text mt={5}>aanes.dev</Text>
+                    <Link href="https://github.com/boaanes" color="#61dafb" isExternal>
+                        GitHub
+                    </Link>
+                </Flex>
+            </Box>
+        </ChakraProvider>
     );
 };
 
